4be38a997d2fc9a8c55ea204399fe97286aecab4
John Pendexter <jpendexter@manifestcorp.com>
2016-11-03 19:19:33 -0400

    Initial commit

---
+++ src/main/java/com/manifest/PersistanceApplication.java
@@ -0,0 +1,12 @@
+package com.manifest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistanceApplication.class, args);
+	}
+}
